6c564a0c4780cb551a7d8c50672adf03577d8979
lmh0870 <lmh0870@naver.com>
2021-09-15 01:22:13 +0900

    Update: updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,39 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      let uglyPassword = null;
+      if (password) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updateUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          password,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updateUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile.",
+        };
+      }
+    },
   },
 };
