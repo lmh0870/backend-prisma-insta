cff79545daa254f3739c683c8a0c4bcaaf652046
lmh0870 <lmh0870@naver.com>
2021-09-16 01:21:17 +0900

    Authentication: Two

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import schema from "./schema";
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   schema,
+  context: {
+    "jwt-token":
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjMxNzIyNDQ4fQ.k79_XOa8GwT9kWMGTOsHhLydcDx49PIlPIroXdeheBc",
+  },
 });
 server
   .listen(PORT)
