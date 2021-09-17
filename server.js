d195dcbacef359862f7c39803fb751cc135434dc
lmh0870 <lmh0870@naver.com>
2021-09-17 19:47:28 +0900

    Upload: One

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
-import schema from "./schema";
+import { typeDefs, resolvers } from "./schema";
+import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
-  schema,
-  context: {
-    "jwt-token":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjMxNzIyNDQ4fQ.k79_XOa8GwT9kWMGTOsHhLydcDx49PIlPIroXdeheBc",
+  resolvers,
+  typeDefs,
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+    };
   },
 });
 server
